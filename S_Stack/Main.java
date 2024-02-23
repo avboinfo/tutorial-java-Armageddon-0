5bc9688c2d007c9eb9499d489b510e58f974afd8
Armageddon-0 <150423650+Armageddon-0@users.noreply.github.com>
2024-02-23 12:58:31 +0000

    piccole modifiche

--- S_Stack/Main.java
+++ S_Stack/Main.java
@@ -11,17 +11,19 @@ public class Main {
     public static void main( String[] args ) {
         Stack<String> s = new Stack<>();
        try{
-        File f=new File("FileDiLettura.csv");
+        File f=new File("S_Stack/FileDiLettura.csv");
         Scanner sc= new Scanner(f);
         while (sc.hasNextLine()) {
             s.push(sc.nextLine());
+            
         }
-       
+         System.out.println(s);
+         sc.close();
        }catch(FileNotFoundException ex){
             System.out.println("Errore"+ex);
        }
        
-       try{
+   /*     try{
       
        File g=new File("FileFinale.csv");
        PrintWriter scrittura= new PrintWriter(g);
@@ -30,7 +32,7 @@ public class Main {
        }
     }catch(FileNotFoundException e){
         System.out.println("Errore"+e);
-       }
+       }*/
        
        
       
